7500c588da9beb6d706b01a876d44355cf414743
wangxin12030726 <18910259163@163.com>
2019-10-29 13:58:08 +0800

    test

--- design-pattern/src/main/java/strategy/demo1/StratebyBImpl.java
+++
@@ -1,8 +0,0 @@
-package strategy.demo1;
-
-public class StratebyBImpl implements  Strategy{
-    public double discount(double price) {
-        System.out.println("会员用户打折9.8折");
-        return price * 0.98;
-    }
-}
